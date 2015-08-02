72649d26e320340d5d10edc64e12e9a5b0e1c291
Richard Walker <digitalsadhu@gmail.com>
2015-08-02 21:56:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var spawn = require('child_process').spawn,
+    parse = require('dokku-git-remote-stream')
+
+module.exports = function (cb) {
+  var gitRemote = spawn('git', ['remote', '-v'], { cwd: process.cwd() })
+
+  gitRemote.stdout
+    .pipe(parse)
+    .on('data', function (data) {
+      cb(data.host, data.appName)
+    })
+}
