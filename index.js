0cdde5002d55bad7bc35daeed96f7449724ab952
Richard Walker <digitalsadhu@gmail.com>
2015-08-16 09:38:14 +0200

    Rewrite module to use git-remotes module

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-var spawn = require('child_process').spawn,
-    parse = require('dokku-git-remote-stream')
+var gitRemotes = require('git-remotes')
+var dokkuUrlRegExp = /dokku@([^:]*):(?:\/.*\/)?([^\s]*)/
 
-module.exports = function (cb) {
-  var gitRemote = spawn('git', ['remote', '-v'], { cwd: process.cwd() })
+module.exports = function dokkuGitRemoteParser(dir, cb) {
+  if (typeof dir === 'function') {
+    cb = dir
+    dir = process.cwd()
+  }
 
-  gitRemote.stdout
-    .pipe(parse)
-    .on('data', function (data) {
-      cb(data.host, data.appName)
-    })
+  gitRemotes(dir, function (err, remotes) {
+    if (err) {
+      var error = new Error('Unable to parse git remotes')
+      error.stack = err.stack
+      return cb(error)
+    }
+
+    var dokkuRemoteUrls = remotes
+      .filter(function (gitRemote) { return gitRemote.url.match(dokkuUrlRegExp) })
+      .map(function (gitRemote) {
+        var gitRemoteUrlMatches = gitRemote.url.match(dokkuUrlRegExp)
+
+        return {
+          host: gitRemoteUrlMatches[1],
+          appName: gitRemoteUrlMatches[2]
+        }
+      })
+
+
+    if (dokkuRemoteUrls.length === 0) return cb(new Error('No Dokku app detected'))
+
+    cb(null, dokkuRemoteUrls[0].host, dokkuRemoteUrls[0].appName)
+  })
 }
